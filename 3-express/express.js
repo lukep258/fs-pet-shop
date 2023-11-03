367109a7b5ef8aa14b3143487aca4cdda24893a6
Luke Park <143543147+lukep258@users.noreply.github.com>
2023-11-03 10:02:47 -0500

    Co-authored-by: drewk93 <drewk93@users.noreply.github.com>

--- 3-express/express.js
+++ 3-express/express.js
@@ -1,24 +1,33 @@
 `use strict`
-
-import fs from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import express, { json } from 'express'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-const petsPath = path.join(__dirname,'../pets.json')
-const port = 8000
-const app = express();
+ // CORE MODULES IMPORT
+import fs from 'fs' // File system manager module
+import path from 'path' // Path module
+import { fileURLToPath } from 'url' // URL module, convert path to URL
 
 
+import express from 'express' // express module import
+
+// GLOBAL VARIABLE DECLARATION
+const app = express(); // Assign express function to app variable
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+const __dirname = path.dirname(fileURLToPath(import.meta.url)) 
+const petsPath = path.join(__dirname,'../pets.json') // Path to pets.json file
+const port = 8000 // Define port for client
+
+
+// PROGRAM START FUNCTION
 const init=()=>{
-    //input validation
+    //input validation - ln37
     URLError()
 
-    //instance
-    serverInstance()
+    //GET instance - ln 53
+    serverInstanceGET()
 
-    //catchAll
+    //POST instance - ln 71
+    serverInstancePOST()
+
+    //catchAll - ln 63
     errorRes()
 
     //listener
@@ -29,50 +38,60 @@ const init=()=>{
 //input Validation
 const URLError=()=>{
     app.use((req,res,next)=>{
-        const route = /^\/pets(.*)$/
-        const petIndex = req.url.match(route)
+        const route = req.url.split('/')
 
-        switch(petIndex[1][0]){
-            case '/':
-            case undefined:
-                next()
-                break
-            default:
-                next({message:'Token not present',status:401})
-        }
+        route[1]==='pets'&&route[2]?
+            parseInt(route[2])>-1?
+                route[3]?
+                    next({message:'invalid endpoint',status:401}): // Send 401 error: client request needs authentication credentials
+                    next():
+                next({message:'incorrect endpoint', status: 402}): 
+            next()
     })
 }
 
 
 //instance
-const serverInstance=()=>{
-    app.get(/^\/pets(.*)$/,(req,res)=>{
-        res.send('good test')
-        const urlInput=req.params
-
+const serverInstanceGET=()=>{
+    app.get(/^\/pets(.*)$/,(req,res,next)=>{
+        console.log('getting')
+        const urlInput=req.params['0']
+        // Invoke fs.readFile to access and convert for client get request
         fs.readFile(petsPath,'utf8',(err,data)=>{
-            const fileArr = JSON.parse(data)
-            console.log(fileArr)
+            const fileArr = JSON.parse(data) //parse to JSON format for get request
+            
+            urlInput?
+                res.send(fileArr[urlInput[1]]):
+                res.send(fileArr)
         })
     })
 }
 
 
-//get request
-// const getPet=(res,route)=>{
-//     const petIndex = route
-//     fs.readFile(petsPath,'utf8',(err,data)=>{
+const serverInstancePOST=()=>{
+    app.post(/^\/pets(.*)$/,(req,res,next)=>{
+        console.log('posting')
 
-//     })
-// }
+        fs.readFile(petsPath,'utf8',(err,data)=>{
+            let petsArr = JSON.parse(data)
+
+            petsArr.push(req.body)
+            petsArr = JSON.stringify(petsArr)
+    
+            fs.writeFile(petsPath,petsArr,()=>{})
+    
+            res.send(req.body)
+        })
+    })
+}
 
 
 //error response
 const errorRes=()=>{
     app.use((err,req,res,next)=>{
-        res.status(err.status).json({err:err.message})
+        res.status(err.status).json({err:err.message,status:err.status})
     })
 }
 
 
-init()
+init() // Run init file and start program
